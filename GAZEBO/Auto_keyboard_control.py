8c02b0908f85bd40e9391079f2bff4e2bbf465bc
wjdgkr526 <73153293+wjdgkr526@users.noreply.github.com>
2020-12-06 18:28:13 +0900

    Create Auto_keyboard_control.py

---
+++ GAZEBO/Auto_keyboard_control.py
@@ -0,0 +1,14 @@
+# GJ-hospital
+
+import pyautogui
+import time
+
+pyautogui.typewrite("cd PX4-Autopilot",interval=0.1)
+pyautogui.press('Enter')
+time.sleep(1)
+pyautogui.press('Enter')
+time.sleep(2)
+pyautogui.typewrite("make px4_sitl_default gazebo",interval=0.1)
+pyautogui.press('Enter')
+time.sleep(1)
+pyautogui.typewrite()
